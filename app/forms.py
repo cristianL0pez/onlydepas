8a16a4e62db6dd8296bc87f82416a0fba4d2fd68
cristian_lopez <cristianlopezvalpo@gmail.com>
2024-05-02 15:12:09 -0400

    commit

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Usuario
+
+class RegistroUsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['username', 'password', 'nombres', 'apellidos', 'rut', 'direccion', 'telefono', 'correo_electronico', 'tipo_usuario']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
---
+++ app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+
+from app.models import SolicitudArriendo
+from .forms import RegistroUsuarioForm
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
+def registro_usuario(request):
+    if request.method == 'POST':
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit=False)
+            password = form.cleaned_data['password']
+            usuario.set_password(password)
+            usuario.save()
+            # Autenticar al usuario después del registro
+            usuario_autenticado = authenticate(username=usuario.username, password=password)
+            if usuario_autenticado is not None:
+                login(request, usuario_autenticado)
+                # Redirigir a alguna página de éxito
+                return redirect('index')  # Cambia 'inicio' a la URL deseada
+    else:
+        form = RegistroUsuarioForm()
+    return render(request, 'registro_usuario.html', {'form': form})
+
+
+
