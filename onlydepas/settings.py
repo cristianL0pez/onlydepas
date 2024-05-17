ef9b8683edd21ee85711dcd90fbac10a446dc530
Cristian López <60026545+cristianL0pez@users.noreply.github.com>
2024-05-17 12:23:38 -0400

    Update settings.py
    tusted

--- onlydepas/settings.py
+++ onlydepas/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*','onlydepas.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['https://onlydepas.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['*','onlydepas.onrender.com']
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'  # URL a la que se redirige si un usuario no está autenticado y trata de acceder a una vista protegida
